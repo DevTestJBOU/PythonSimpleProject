5de60bf740496c9a8d2a2241683a74216e9c3b32
DevTestJBOU <46719520+DevTestJBOU@users.noreply.github.com>
2019-02-06 14:45:29 +0000

    Update helloworld_finished.py

--- helloworld_finished.py
+++ helloworld_finished.py
@@ -1,7 +1,29 @@
---- HelloWorld ---
+#
+#Simple Python script to add some numbers 
+#
+print ("# Julian Burdett")
+print ("# Simple noddy Python sum script ") 
+print("")
 
-def main():
-  print ("hello world!")
-    
-if __name__ == "__main__":
-  main()
+numbers = [2.5, 3, 4, -5]
+
+# start parameter is not provided
+numbersSum1 = sum(numbers)
+print ("The 1st result is:") 
+
+print(numbersSum1) #result should be 4.5
+print("")
+
+print ("# Julian Burdett")
+print ("# Simple noddy Python sum script ") 
+print("")
+
+numbers = [2.5, 3, 4, -5]
+
+# start parameter is not provided
+numbersSum2 = sum(numbers)
+# start = 10
+numbersSum2 = sum(numbers, 10)#print(numbersSum)
+
+print ("The 2nd result is:")
+print (numbersSum2) #result should be 14.5
